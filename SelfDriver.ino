c345024f5f82811b63214caff59db7edad4ffe05
Alexander Dada <alexdada555@gmail.com>
2018-06-09 17:27:05 +0000

    Add files via upload

--- SelfDriver.ino
+++ SelfDriver.ino
@@ -17,7 +17,7 @@
 #define ECHO_PIN   4 // Arduino pin tied to echo pin on the ultrasonic sensor.
 #define TRIGGER_PIN2  7  // Arduino pin tied to trigger pin on the ultrasonic sensor.
 #define ECHO_PIN2     8  // Arduino pin tied to echo pin on the ultrasonic sensor.
-#define MAX_DISTANCE 500 // Maximum distance ping (in centimeters). Maximum sensor distance is rated at 400-500cm.
+#define MAX_DISTANCE 400 // Maximum distance ping (in centimeters). Maximum sensor distance is rated at 400-500cm.
 
 //--------------------------------------------------------------------------------------------------------------------
 /*
@@ -37,9 +37,10 @@ MPU6050 mpu;
  
 double z = 0,zz = 0;
 
-int e = 0,a  = 0,aT  = 0,b  = 0,bT  = 0;
+int e = 0,a  = 0,aT  = 0,b  = 0,bT  = 0,N = 20,NT;
 int f  = 0,fT  = 0,g  = 0,gT  = 0,prevError  = 0,E1 = 0,E2  = 0;
 int PositionSetpoint = 0,angleSetpoint = 0,initVel = 80;
+int PosLimit = 200, AngLimit = 500;
 
 unsigned int MR,ML;
 unsigned long timer = 0;
@@ -47,6 +48,7 @@ unsigned long timer = 0;
 float timeStep = 0.01, gyaw = 0,pi = 3.143;
 
 bool LimitDgain = true,LimitIgain = true;
+
 //--------------------------------------------------------------------------------------------------------------
 /*
  *                                   COMPONENT INITIALISATION LOOP 
@@ -77,13 +79,16 @@ void setup()
  */
 //------------------------------------------------------------------------------------------------------------------
  
-                      double prop = 5.3,inte = 2.8,deriv = 3;   // PID GAINS
+                      double prop = 5.3,inte = 2.9,deriv = 1;   // PID GAINS
+                      //double prop = 615.5,inte = 30.8,deriv = 5;
 
 void loop()
 { 
   
    ML = 0;
    MR = 0;
+   
+   
    
    timer = millis();
    Vector norm = mpu.readNormalizeGyro();            // read in gyro data as a 3x1 vector
@@ -102,27 +107,29 @@ void loop()
    //Serial.println(zz);
    //Serial.print("\t | \t ");
    
-  if (zz > 0)                    //if angle > 0....
-  {
-    f = zz;
-    fT += f;                            // calculate sum error (for integral)
-    MR = pid(f,fT,prop,inte,deriv,LimitIgain,LimitDgain);     // calculate PID gain
-    RunMotors(9,MR/500,initVel);          
-    RunMotors(10,-MR/500,initVel);      // run motors
-  }
-  else if (zz < 0)                //if angle < 0....
-  {
-    g = -1*zz;
-    gT += g;                            // calculate sum error (for integral)
-    ML = pid(g,gT,prop,inte,deriv,LimitIgain,LimitDgain);     // calculate PID gain
-    RunMotors(10,ML/500,initVel);
-    RunMotors(9,-ML/500,initVel);       // run motors
-  }
-  else
-  {
-    RunMotors(10,0,initVel);
-    RunMotors(9,0,initVel);
-  }
+   if (zz > 0)                    //if angle > 0....
+   {
+       f = zz;
+       fT += f;                            // calculate sum error (for integral)
+       NT += N;
+       MR = pid(f,fT,prop,inte,deriv,LimitIgain,LimitDgain,N,NT);     // calculate PID gain
+       RunMotors(9,MR/AngLimit,initVel);          
+       RunMotors(10,-MR/AngLimit,initVel);      // run motors
+   }
+   else if (zz < 0)                //if angle < 0....
+   {
+       g = -1*zz;
+       gT += g;                            // calculate sum error (for integral)
+       NT += N;
+       ML = pid(g,gT,prop,inte,deriv,LimitIgain,LimitDgain,N,NT);     // calculate PID gain
+       RunMotors(10,ML/AngLimit,initVel);
+       RunMotors(9,-ML/AngLimit,initVel);       // run motors
+   }
+   else
+   {
+       RunMotors(10,0,initVel);
+       RunMotors(9,0,initVel);
+   }
  
    E1 = sonar.ping_cm();
    E2 = sonar2.ping_cm();                        // read in distance from untrasonic sensors
@@ -138,24 +145,26 @@ void loop()
   
    if (e > 0)                 //if angle > 0....
    {
-     a = e;
-     aT += a;                          // calculate sum error (for integral)
-     MR = pid(a,aT,prop,inte,deriv,LimitIgain,LimitDgain);   // calculate PID gain
-     RunMotors(9,MR/250,initVel);
-     RunMotors(10,-MR/250,initVel);     // run motors
+      a = e;
+      aT += a;                          // calculate sum error (for integral)
+      NT += N;
+      MR = pid(a,aT,prop,inte,deriv,LimitIgain,LimitDgain,N,NT);   // calculate PID gain
+      RunMotors(9,MR/PosLimit,initVel);
+      RunMotors(10,-MR/PosLimit,initVel);     // run motors
    }
    else if (e < 0)          //if angle < 0....
    {
-     b = -1*e;
-     bT += b;                         // calculate sum error (for integral)
-     ML = pid(b,bT,prop,inte,deriv,LimitIgain,LimitDgain);  // calculate PID gain
-     RunMotors(10,ML/250,initVel);     
-     RunMotors(9,-ML/250,initVel);     // run motors
+      b = -1*e;
+      bT += b;                         // calculate sum error (for integral)
+      NT += N;
+      ML = pid(b,bT,prop,inte,deriv,LimitIgain,LimitDgain,N,NT);  // calculate PID gain
+      RunMotors(10,ML/PosLimit,initVel);     
+      RunMotors(9,-ML/PosLimit,initVel);     // run motors
    }
    else
    {
-     RunMotors(10,0,initVel);
-     RunMotors(9,0,initVel);
+      RunMotors(10,0,initVel);
+      RunMotors(9,0,initVel);
    }
 }
 
@@ -171,46 +180,45 @@ void loop()
   
 int error(int a, int b, int c)
 {
-  int d;
-  d = c - (a - b);
-  return(d);
+    int d;
+    d = c - (a - b);
+    return(d);
 }
 
 /*
  *                                      CALCULATING THE PID GAIN VALUES
  */
  
-double pid(int InputError,int InputErrorTotal,double Kp,double Ki,double Kd,bool Ilim,bool Dlim)
+double pid(int InputError,int InputErrorTotal,double Kp,double Ki,double Kd,bool Ilim,bool Dlim,int N,int NT)
 {
-  double p=0,i=0,d=0,cont=0;
-  double N=22,NT;
-  NT += N;      //Practical Derivitive Term components(Anti-High Frequency Noise Sensitvity)
-  double ad = Kd/(Kd+NT),bd = Kp*N*ad;
+   double p=0,i=0,d=0,cont=0;
+   //Practical Derivitive Term components(Anti-High Frequency Noise Sensitvity)
+   double ad = Kd/(Kd+NT),bd = Kp*N*ad;
   
-  p = InputError*Kp;
+    p = InputError*Kp;
   
-  if (Ilim == true)
-  {
-     i = (InputErrorTotal*Ki) + (35*(255 - cont));
-  }
-  else
-  {
-    i = InputErrorTotal*Ki;
-  }
+    if (Ilim == true)
+    {
+       i = (InputErrorTotal*Ki) + (30*(255 - cont));
+    }
+    else
+    {
+       i = InputErrorTotal*Ki;
+    }
   
-  if (Dlim == true)
-  {
-     d = ad*(d-bd)*(InputError-prevError);
-  }
-  else
-  {
-     d = Kd*(InputError-prevError);
-  }
+    if (Dlim == true)
+    {
+       d = ad*(d-bd)*(InputError-prevError);
+    }
+    else
+    {
+       d = Kd*(InputError-prevError);
+    }
  
-  prevError = InputError;
+    prevError = InputError;
   
-  cont = p + i + d;
-  return(cont);
+    cont = p + i + d;
+    return(cont);
 }
 
 /*
@@ -219,15 +227,15 @@ double pid(int InputError,int InputErrorTotal,double Kp,double Ki,double Kd,bool
  
 void RunMotors(int Motor,int Gain,int Normal)
 {
-  int x = 0;
-  if((Gain+Normal) > 255)
-  {
-    x = 255;                      // Actuator Limit Saturation 
-  }
-  else
-  {
-    x = Gain+Normal;              // add the PID gain to the initial velocity
-  }
-  analogWrite(Motor,x);
+    int x = 0;
+    if((Gain+Normal) > 255)
+    {
+        x = 255;                      // Actuator Limit Saturation 
+    }
+    else
+    {
+        x = Gain+Normal;              // add the PID gain to the initial velocity
+    }
+    analogWrite(Motor,x);
 }
 
