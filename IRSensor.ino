02fc494603db8f0a438ac09b97599a5675a32feb
Alexander Dada <alexdada555@gmail.com>
2018-09-29 03:15:51 +0100

    Add files via upload

---
+++ IRSensor.ino
@@ -0,0 +1,36 @@
+void setup() 
+{
+  Serial.begin(9600);
+}
+
+void loop() 
+{
+  int Distance,Distance2;
+  
+  Distance = IRSensorDistance(A0);
+  Distance2 = IRSensorDistance(A1);
+}
+
+int IRSensorDistance(int Sensor)
+{
+  int distance;
+  float sensorValue = analogRead(Sensor)*0.0048828125;
+  float IR,NewIR;
+  
+  for(int i = 0;i < 10;i++)
+  {
+    IR += sensorValue;
+  }
+  NewIR = IR/10;
+  
+  distance = 13*pow(NewIR,-1);
+  
+  if(distance <= 30)
+  {
+    return(distance);
+  }
+  else
+  {
+    return(0);
+  }
+}
